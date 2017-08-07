dde2d335a481ec0c6e27c3d299bdf915124dfb82
jakemagner <jakemagner90@gmail.com>
2017-08-06 22:51:26 -0700

    Change DrawCardsInstruction to check draw pile size before drawing. Test it.

--- tests/draw_cards_instruction_test.py
+++ tests/draw_cards_instruction_test.py
@@ -65,9 +65,75 @@ class DrawCardsInstructionTest(unittest.TestCase):
         )
         self.assertEqual(
             cards_in_hand_after_draw - number_to_draw, cards_in_hand_before_draw,
-            'Players hand pile is larger by the number of drawn cards.'
+            'Players hand is larger by the number of drawn cards.'
         )
         self.assertTrue(logger.has_logged_msg('%s drew 5 cards.' % current_player))
+
+    def test_draw_pile_runs_out_but_not_discard_pile(self):
+        game_state = self.create_game_state()
+        player_state = game_state.get_player_state('p1')
+        for i in range(10):
+            player_state.discard.add(0)
+        cards_in_discard_before_draw  = player_state.discard.size()
+        cards_in_draw_pile_before_draw = player_state.draw_pile.size()
+        cards_in_hand_before_draw = player_state.hand.size()
+        number_greater_than_draw_pile_size = cards_in_draw_pile_before_draw + 5
+        number_to_draw = number_greater_than_draw_pile_size
+        instruction = DrawCardsInstruction(
+            number_to_draw=number_to_draw, target_player='p1'
+        )
+        logger = TestLogger()
+        instruction.execute(game_state, logger=logger)
+        cards_in_draw_pile_after_draw = player_state.draw_pile.size()
+        cards_in_hand_after_draw = player_state.hand.size()
+        cards_in_discard_after_draw  = player_state.discard.size()
+        self.assertEqual(
+            cards_in_draw_pile_before_draw + cards_in_discard_before_draw - number_to_draw,
+            cards_in_draw_pile_after_draw,
+            'Players draw pile should be the remainder of the discard pile after shuffling it '
+            'in and finishing the draw.'
+        )
+        self.assertEqual(
+            cards_in_hand_after_draw - number_to_draw,
+            cards_in_hand_before_draw,
+            'Players hand is larger by the number of drawn cards.'
+        )
+
+    def test_draw_pile_runs_out_and_discard_pile_runs_out(self):
+        game_state = self.create_game_state()
+        player_state = game_state.get_player_state('p1')
+        for i in range(10):
+            player_state.discard.add(0)
+        cards_in_discard_before_draw  = player_state.discard.size()
+        cards_in_draw_pile_before_draw = player_state.draw_pile.size()
+        cards_in_hand_before_draw = player_state.hand.size()
+        number_greater_than_both_piles = (
+            cards_in_draw_pile_before_draw + cards_in_discard_before_draw + 100
+        )
+        number_to_draw = number_greater_than_both_piles
+        instruction = DrawCardsInstruction(
+            number_to_draw=number_to_draw, target_player='p1'
+        )
+        logger = TestLogger()
+        instruction.execute(game_state, logger=logger)
+        cards_in_draw_pile_after_draw = player_state.draw_pile.size()
+        cards_in_hand_after_draw = player_state.hand.size()
+        cards_in_discard_after_draw  = player_state.discard.size()
+        self.assertEqual(
+            0,
+            cards_in_draw_pile_after_draw,
+            'Players draw pile should be empty if they tried to draw more than the draw and '
+            'discard piles combined'
+        )
+        self.assertEqual(
+            cards_in_hand_after_draw,
+            (
+                cards_in_hand_before_draw +
+                cards_in_draw_pile_before_draw +
+                cards_in_discard_before_draw
+            ),
+            'Players hand is larger by the number of cards they drew.'
+        )
 
 
 if __name__ == '__main__':
