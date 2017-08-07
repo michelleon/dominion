b51d1f85d39b514703c0e45e421bf2e39051ffde
jakemagner <jakemagner90@gmail.com>
2017-08-06 17:31:38 -0700

    Add PlayerState

---
+++ core/player_state.py
@@ -0,0 +1,23 @@
+from core.card_distribution import CardDistribution
+from core.card_stack import CardStack
+
+
+class PlayerState(object):
+	def __init__(self, name, starting_cards):
+		self._name = name
+		self.draw_pile = CardStack(starting_cards)
+		self.discard = CardStack()
+		self.in_play = CardDistribution()
+		self.hand = CardDistribution()
+		self.actions_left = 1
+		self.buys_left = 1
+
+	def deepcopy(self):
+		new_state = PlayerState(self._name, [])
+		new_state.draw_pile = self.draw_pile.deepcopy()
+		new_state.discard = self.discard.deepcopy()
+		new_state.in_play = self.in_play.deepcopy()
+		new_state.hand = self.hand.deepcopy()
+		new_state.actions_left = self.actions_left
+		new_state.buys_left = self.buys_left
+		return new_state
