e1c12afd3c062522d2ab2720b4df1516926e0681
jakemagner <jakemagner90@gmail.com>
2017-08-18 17:33:04 -0700

    Add UnorderedCardStack. Add more methods to CardStack (#16)
    * Add UnorderedCardStack. Add more methods to CardStack
    
    * Fix bug in adding to bottom of stack
    
    * Add Location and CounterId (#19)
    
    Adding `Location` and `CounterId` classes. These represent parts of the game state. Locations are places where card stacks are stored (trash, p1's hand, p1's discard, etc.) and CounterId's represent integer counters (number of buys remaining for example)

--- core/card_distribution.py
+++ core/card_distribution.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from copy import deepcopy
 
+
 class CardDistribution:
     """
     Represents cards to counts mapping
@@ -9,14 +10,32 @@ class CardDistribution:
         initial_card_list = initial_card_list or []
         self._card_counter = Counter(initial_card_list)
 
+    def __iter__(self):
+        for card, count in self.cards_to_counts().items():
+            for i in range(count):
+                yield card
+
+    def __repr__(self):
+        s = 'CardDistribution\n'
+        card_strs = ['%s:\t%d' % (str(key), val) for key, val in self._card_counter.items()]
+        s += '\n'.join(card_strs)
+        s += '\n'
+        return s
+
     def count(self, card):
         return self._card_counter[card]
 
     def cards_to_counts(self):
         return dict(self._card_counter)
 
+    def empty(self):
+        self._card_counter = Counter()
+
     def add(self, card, amount = 1):
         self._card_counter[card] += amount
+
+    def size(self):
+        return sum(self._card_counter.values())
 
     def subtract(self, card, amount = 1):
         self._card_counter.subtract({ card: amount })
--- core/card_stack.py
+++ core/card_stack.py
@@ -1,8 +1,18 @@
 from collections import deque
-from random import shuffle
 from copy import deepcopy
+from random import shuffle
+import enum
 
 from core.card_distribution import CardDistribution
+
+
+class StackPosition(enum.Enum):
+    """
+    References a position in a card stack.
+    """
+    TOP = 0
+    BOTTOM = 1
+
 
 class CardStack:
     """
@@ -13,22 +23,58 @@ class CardStack:
         self.distribution = CardDistribution(initial_card_list)
         self._stack = deque(initial_card_list)
 
-    # Use case: gain a card
-    def add(self, card):
-        self.distribution.add(card)
-        self._stack.appendleft(card)
+    def __iter__(self):
+        for card in self._stack:
+            yield card
 
-    # Use case: when removing from hand or picking from discard
-    def extract(self, card):
-        self.distribution.subtract(card)
-        self._stack.remove(card)
-        return card
+    def add(self, cards, position=None):
+        """
+        Add a list of cards to the stack at the position. Position default to the top of stack.
+        Note that `cards` can be a list or single item. If it's a single item it will be
+        treated as if it were the only item in a list.
+
+        If there are multiple cards in `cards` then they will be added such that the first card is
+        higher in the stack than the last element.
+
+        Parameters:
+            cards (list): List of cards to add.
+            position (optional, `StackPosition`): Position to add the cards to the stack.
+        """
+        position = position or StackPosition.TOP
+        if type(cards) != list:
+            cards = [cards]
+        for card in cards:
+            self.distribution.add(card)
+        if position == StackPosition.TOP:
+            # Have to reverse cards because extendleft reverses them
+            self._stack.extendleft(cards[::-1])
+        else:
+            self._stack.extend(cards)
+
+    def extract(self, cards):
+        """
+        Remove the cards from the stack and return them. If there are multiple of one of the cards
+        in the stack then the one closest to the top will be extracted.
+        """
+        for card in cards:
+            self.distribution.subtract(card)
+            self._stack.remove(card)
+        return cards
+
+    def draw(self, amount, from_position=None):
+        cards = []
+        for i in range(amount):
+            card = self._stack.popleft()
+            cards.append(card)
+            self.distribution.subtract(card)
+        return cards
 
     def has_card(self, card):
         return self.distribution.count(card) > 0
 
-    def draw(self, amount):
-        return [self._stack.popleft() for i in range(amount)]
+    def empty(self):
+        self._stack = deque()
+        self.distribution  = CardDistribution()
 
     def shuffle(self):
         shuffle(self._stack)
@@ -43,5 +89,46 @@ class CardStack:
         return card_stack
 
 
+class UnorderedCardStack(CardStack):
+    """
+    An unordered stack of cards. When an ordering is specified it will just be ignored.
+    """
+    def __init__(self, initial_card_list=None):
+        initial_card_list = initial_card_list or []
+        self.distribution = CardDistribution(initial_card_list)
 
-        
+    def __iter__(self):
+        for card, count in self.distribution.cards_to_counts().items():
+            for i in range(count):
+                yield card
+
+    def add(self, cards, position=None):
+        if type(cards) != list:
+            cards = [cards]
+        for card in cards:
+            self.distribution.add(card)
+
+    def draw(self, amount, position=None):
+        raise NotImplementedError('Can not draw from UnorderedCardStack')
+
+    def empty(self):
+        self.distribution.empty()
+
+    def extract(self, cards):
+        for card in cards:
+            self.distribution.subtract(card)
+        return cards
+
+    def has_card(self, card):
+        return self.distribution.count(card) > 0
+
+    def shuffle(self):
+        return        
+
+    def size(self):
+        return self.distribution.size()
+
+    def deepcopy(self):
+        card_stack = UnorderedCardStack()
+        card_stack.distribution = self.distribution.deepcopy()
+        return card_stack
