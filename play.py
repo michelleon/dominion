ea564dbe8307f8b8c364afde6ca9b5ee48dabfe8
jakemagner <jakemagner90@gmail.com>
2017-08-19 21:07:04 -0700

    Make default agent a better one

--- play.py
+++ play.py
@@ -28,8 +28,8 @@ if __name__ == '__main__':
 	parser.add_argument("--p1", help="Full path to agent to play as player 1")
 	parser.add_argument("--p2", help="Full path to agent to play as player 2")
 	args = parser.parse_args()
-	p1_path = args.p1 or 'core.agents.test.TestAgent'
+	p1_path = args.p1 or 'core.agents.big_money.SimpleBmSmithyAgent'
 	p1_agent = _str_to_agent_class(p1_path)('p1')
-	p2_path = args.p2 or 'core.agents.test.TestAgent'
+	p2_path = args.p2 or 'core.agents.big_money.SimpleBmSmithyAgent'
 	p2_agent = _str_to_agent_class(p2_path)('p2')
 	play_game(p1_agent, p2_agent)
